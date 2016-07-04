d00f683fb686ad9085248d8cd2728cd38a184b22
NeethuV199 <vneethu199@gmail.ocm>
2016-07-04 15:19:53 +0530

    k

---
+++ Exivision/Exivision/PageObjects/Frontend.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exivision.PageObjects
+{
+    class FrontEnd
+    {
+        #region Login
+        //Login elements
+        public string SigninLink = "aSignIn";
+        public string UserNameField = "txtUserName";
+        public string PasswordField = "txtPassword";
+        public string ClickOnLogin = "BtnUserLogin";
+        #endregion
+
+        #region ForgotPasswordLink
+        //ForgotPassword elements
+        public string ForgotPasswordId = "aForgotPassword";
+        #endregion
+
+        #region AccountRequestFromSignin
+        //AccountRequest elements
+        public string AccountRequestId = "aCreateAccount";
+        #endregion
+    }
+}
